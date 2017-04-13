af805027c0fe35bf574dc7b1684d20fbb10aa279
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-13 23:01:01 +0100

    fixed error caused by having two export defaults
    Delete the first 'export default' in front of class App.

--- 002 - Basics of Redux Thunk/src/components/app.js
+++ 002 - Basics of Redux Thunk/src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-export default class App extends Component {
+class App extends Component {
   componentWillMount() {
     this.props.fetchUsers();
   }
